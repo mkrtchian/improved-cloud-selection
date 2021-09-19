3b88de52b0f30a97d489604c59c6f721b9421358
Roman Mkrtchian <mkrtchia@gmail.com>
2021-09-19 12:33:42 +0200

    maint: add git pre commit hooks for both frontend and backend with pre-commit (#6)

--- frontend/test/pages/index.test.tsx
+++ frontend/test/pages/index.test.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { render, fireEvent } from "../testUtils";
+import { render } from "../testUtils";
 import { Home } from "../../pages/index";
 
 describe("Home page", () => {
-  it("clicking button triggers alert", () => {
+  it("displays hello world", () => {
     const { getByText } = render(<Home />, {});
-    window.alert = jest.fn();
-    expect(getByText("Test Button")).toBeInTheDocument();
-    fireEvent.click(getByText("Test Button"));
-    expect(window.alert).toHaveBeenCalledWith("With typescript and Jest");
+    expect(getByText("Hello world!")).toBeInTheDocument();
   });
 });
