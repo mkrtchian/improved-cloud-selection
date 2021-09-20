ff58bc11f9a896feadf16e2e2e5833f1818e221b
Roman Mkrtchian <mkrtchia@gmail.com>
2021-09-21 01:18:53 +0200

    convert e2e tests to typescript

--- frontend/codecept.conf.js
+++ frontend/codecept.conf.js
@@ -2,10 +2,10 @@ const { setHeadlessWhen } = require("@codeceptjs/configure");
 
 setHeadlessWhen(process.env.CI);
 
-var server = require("./end_to_end/end_to_end_server");
+var server = require("./e2e/config/end_to_end_server");
 
 exports.config = {
-  tests: "./e2e/*_test.js",
+  tests: "./e2e/*_test.ts",
   output: "./e2e/output",
   helpers: {
     Playwright: {
