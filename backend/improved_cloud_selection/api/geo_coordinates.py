2cd17ba36cf3a63fed01a1cfec08916a53a65603
Roman Mkrtchian <mkrtchia@gmail.com>
2021-09-23 11:15:44 +0200

    feat: make the backend send the client ip to external geo provider

--- backend/improved_cloud_selection/api/geo_coordinates.py
+++ backend/improved_cloud_selection/api/geo_coordinates.py
@@ -1,16 +1,21 @@
+import logging
+
 import requests
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 
 from improved_cloud_selection.config import Settings, get_settings
 
 router = APIRouter()
 
+log = logging.getLogger("uvicorn")
+
 
 @router.get("/geo-coordinates")
-def geo_coordinates(settings: Settings = Depends(get_settings)):
+def geo_coordinates(request: Request, settings: Settings = Depends(get_settings)):
     """Geo coordinates of the user making the request, from his IP.
 
     Args:
+        request: the incoming request.
         settings: the main settings.
 
     Returns:
@@ -20,12 +25,14 @@ def geo_coordinates(settings: Settings = Depends(get_settings)):
         HTTPException: in the case where the provider replies with an error.
 
     """  # noqa: DAR402
-    response = make_request(settings.geo_ip_provider_url)
+    response = make_request(f"{settings.geo_ip_provider_url}/{request.client.host}")
     check_response_status(response)
     geo_data = response.json()
     result_data = {}
     result_data["longitude"] = geo_data["longitude"]
     result_data["latitude"] = geo_data["latitude"]
+    result_data["ip"] = request.client.host
+    result_data["headers"] = request.headers
     return result_data
 
 
