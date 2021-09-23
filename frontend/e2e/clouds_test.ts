8b7448e1aa1e57c26a814a9dac4a9393e842356f
Roman Mkrtchian <mkrtchia@gmail.com>
2021-09-23 02:38:04 +0200

    update end to end test

--- frontend/e2e/clouds_test.ts
+++ frontend/e2e/clouds_test.ts
@@ -3,7 +3,7 @@ Feature("clouds");
 Scenario("Load page and see AWS regions", ({ I }) => {
   I.amOnPage("/");
   I.see("Enhanced cloud selection");
-  I.see("Africa");
+  I.see("Asia");
 });
 
 export {};
