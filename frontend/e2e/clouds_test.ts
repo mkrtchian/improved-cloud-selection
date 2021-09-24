fdb619e436c3e0955989e2177c23bd2757254bb5
Roman Mkrtchian <mkrtchia@gmail.com>
2021-09-24 02:01:31 +0200

    test: better end to end test

--- frontend/e2e/clouds_test.ts
+++ frontend/e2e/clouds_test.ts
@@ -1,9 +1,19 @@
-Feature("clouds");
+Feature("Clouds");
 
-Scenario("Load page and see AWS regions", ({ I }) => {
+Scenario("Load page and see clouds while clicking on regions", ({ I }) => {
   I.amOnPage("/");
   I.see("Enhanced cloud selection");
-  I.see("Asia");
+  I.click("Google Cloud Platform");
+  I.click("Asia");
+  I.see("Asia, Korea - Google Cloud: Seoul");
+  I.click("Canada");
+  I.see("Canada, Ontario - Google Cloud: Toronto");
+  I.dontSee("Asia, Korea - Google Cloud: Seoul");
+  I.click("Amazon Web Services");
+  I.click("Europe");
+  I.see("Europe, England - Amazon Web Services: London");
+  I.dontSee("Canada, Ontario - Google Cloud: Toronto");
+  I.dontSee("Asia, Korea - Google Cloud: Seoul");
 });
 
 export {};
